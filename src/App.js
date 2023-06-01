fcd2edbc9e721dde6d76d0f5e62b259cb73a1f51
marcelo saraiva <marcelossaraiva8@gmail.com>
2023-06-01 07:59:09 -0300

    000

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import { useState } from "react";
+
+function App() {
+
+  const [city, setCity] = useState("")
+
+const [weatherForecast, setWeatherForecast] = useState(null)
+
+  const handleChange = (e) => {
+
+    setCity(e.target.value)
+  }
+
+  const handleSearch = () => {
+    fetch(`http://api.weatherapi.com/v1/current.json?key=92bc34dd84cf48e4b6a223423233105&q=$(city)&lang=pt`)
+
+      .then((response) => {
+        if(response.status === 200){
+          return response.json()
+        }
+
+      })
+      .then((data )   => { 
+
+        setWeatherForecast(data)
+      } );
+  };
+
+
+  return (
+    <div >
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+
+        <a className="navbar-brand text-white" href="#top">
+          Previsão do tempo12
+        </a>
+
+      </nav>
+
+      <main className="container">
+        <div className="jumbotron">
+          <h1>
+            Verifique o tempo do seu local atual
+          </h1>
+          <p className="lead">
+            Digite o nome da sua cidade no campo abaixo:
+          </p>
+
+          <div className="row mb-4">
+            <div className="col-md-6">
+              <input onChange={handleChange} className="form-control" value={city} />
+            </div>
+          </div>
+          <button onClick={handleSearch} className="btn btn-primary btn-lg ">
+            Pesquisar
+          </button>
+          
+{
+weatherForecast ? (
+<div>
+               <div className="mt-4  ">
+                   <div>
+  <img src= {weatherForecast.current.condition.icon}/>
+</div>
+
+
+<div>
+  <h3>Hoje o dia está:{weatherForecast.current.condition.text}</h3>
+          </div>
+
+          <p className="lead">
+            temperatura: {weatherForecast.current.temp_c}
+          </p>
+</div>
+</div>
+) : null}
+        </div>
+
+   </main>
+    </div>
+  );
+}
+
+export default App;
